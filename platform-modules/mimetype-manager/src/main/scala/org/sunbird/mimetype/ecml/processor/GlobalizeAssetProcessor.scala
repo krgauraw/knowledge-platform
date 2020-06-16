bcdc971fa7b0e501dccf4d9aca647f8110348de3
Kumar Gauraw <er.krgauraw@gmail.com>
2020-06-16 23:13:28 +0530

    updated code

--- platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/GlobalizeAssetProcessor.scala
+++ platform-modules/mimetype-manager/src/main/scala/org/sunbird/mimetype/ecml/processor/GlobalizeAssetProcessor.scala
@@ -15,16 +15,13 @@ trait GlobalizeAssetProcessor extends IProcessor {
 
     val ASSET_DIR:String = "cloud_storage.asset.folder"
     val OBJECT_DIR:String = "cloud_storage.content.folder"
+    val BASE_URL: String = Platform.getString("content.media.base_url", "https://dev.sunbirded.org")
     val timeout: Long = if(Platform.config.hasPath("asset.max_upload_time")) Platform.config.getLong("asset.max_upload_time") else 60
 
     abstract override def process(ecrf: Plugin)(implicit ss: StorageService): Plugin = {
-        println("GlobalizeAssetProcessor ::: process ::: start")
         val manifest = ecrf.manifest
-        println("manifest :::: "+manifest)
         val updatedMedias:List[Media] = uploadAssets(manifest.medias)
-        println("updatedMedias :::: "+updatedMedias)
         val updatedManifest:Manifest = Manifest(manifest.id, manifest.data, manifest.innerText, manifest.cData, updatedMedias)
-        println("updatedManifest :::: "+updatedManifest)
         super.process(Plugin(ecrf.id, ecrf.data, ecrf.innerText, ecrf.cData, ecrf.childrenPlugin, updatedManifest, ecrf.controllers, ecrf.events))
     }
 
@@ -33,28 +30,15 @@ trait GlobalizeAssetProcessor extends IProcessor {
             val future: Future[List[Media]] = Future.sequence(medias.filter(media => StringUtils.isNotBlank(media.id) && StringUtils.isNotBlank(media.src) && StringUtils.isNotBlank(media.`type`))
               .map(media => {
                   Future {
-                      println("media src ::: " + media.src)
                       val file: File = {
                           if (widgetTypeAssets.contains(media.`type`)) new File(getBasePath() + File.separator + "widgets" + File.separator + media.src)
                           else new File(getBasePath() + File.separator + "assets" + File.separator + media.src)
                       }
-                      if (null != file)
-                          println("file path ::: " + file.getAbsolutePath)
-                      /*val cloudDirName = {
-						  val assetDir = if(Platform.config.hasPath(ASSET_DIR)) Platform.config.getString(ASSET_DIR) else System.currentTimeMillis()
-						  Platform.config.getString(OBJECT_DIR) + File.separator + Slug.makeSlug(getIdentifier(), true) + assetDir
-					  }*/
                       val mediaSrc = media.data.getOrElse("src", "").asInstanceOf[String]
-                      println("mediaSrc :::: " + mediaSrc)
                       val cloudDirName = if (!(mediaSrc.startsWith("http"))) FilenameUtils.getFullPathNoEndSeparator(mediaSrc).replace("assets/public/","").replace("content-plugins/","").trim else mediaSrc
-                      println("cloudDirName ::: " + cloudDirName)
-                      //TODO: take it from config
-                      val baseUrl = "https://dev.sunbirded.org"
                       val blobUrl = if (!(mediaSrc.startsWith("http"))) {
-                          if(mediaSrc.startsWith("/")) baseUrl + mediaSrc else  baseUrl + File.separator + mediaSrc
+                          if(mediaSrc.startsWith("/")) BASE_URL + mediaSrc else  BASE_URL + File.separator + mediaSrc
                       }else mediaSrc
-                      println("blobUrl :::: " + blobUrl)
-                      //val uploadFileUrl: Array[String] = ss.uploadFile(cloudDirName, file)
                       val uploadFileUrl: Array[String] = if (StringUtils.isNoneBlank(cloudDirName) && getBlobLength(blobUrl) == 0) ss.uploadFile(cloudDirName, file)
                       else new Array[String](1)
                       if (null != uploadFileUrl && uploadFileUrl.size > 1) {
@@ -68,7 +52,7 @@ trait GlobalizeAssetProcessor extends IProcessor {
                   }
               }))
             val mediaList: List[Media] = Await.result(future, Duration.apply(timeout, "second"))
-            println("mediaList :::: " + mediaList)
+
             if (null != mediaList && !mediaList.isEmpty)
                 mediaList
             else medias
@@ -76,7 +60,6 @@ trait GlobalizeAssetProcessor extends IProcessor {
     }
 
     def getBlobLength(url:String):Long = {
-        println("url for head call ::: "+url)
         val response = Unirest.head(url).header("Content-Type", "application/json").asString
         if (response.getStatus == 200 && null!=response.getHeaders) {
             val size : Long = if(response.getHeaders.containsKey("Content-Length")) response.getHeaders.get("Content-Length").get(0).toLong
